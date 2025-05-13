6e03731c1d6359b748845d58145471511c0afa13
Dinh Duong <jenny.180820@gmail.com>
2025-05-14 04:36:48 +0700

    done NavGraph

--- app/src/main/java/com/fourstars/fourstars_english/MainActivity.kt
+++ app/src/main/java/com/fourstars/fourstars_english/MainActivity.kt
@@ -1,47 +1,196 @@
 package com.fourstars.fourstars_english
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.fourstars.fourstars_english.repository.GoogleAuthRepository
+import com.fourstars.fourstars_english.ui.theme.Feather
 import com.fourstars.fourstars_english.ui.theme.FourstarsenglishTheme
+import com.fourstars.fourstars_english.ui.theme.Nunito
+import com.fourstars.fourstars_english.viewModel.TextToSpeechViewModel
+import com.google.firebase.FirebaseApp
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class MainActivity : ComponentActivity() {
+class MainActivity() : ComponentActivity() {
+
+    private lateinit var googleAuthRepo: GoogleAuthRepository
+    private lateinit var navController: NavHostController
+    private lateinit var textToSpeechViewModel: TextToSpeechViewModel
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        googleAuthRepo = GoogleAuthRepository(this)
+
+        textToSpeechViewModel = ViewModelProvider(this).get(TextToSpeechViewModel::class.java)
+
+        FirebaseApp.initializeApp(this)
+
         setContent {
-            FourstarsenglishTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            navController = rememberNavController() // ✅ Khởi tạo đúng cách
+            MyApp(navController)
         }
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp(navController: NavHostController) {
+    AppNavGraph(navController)
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    FourstarsenglishTheme {
-        Greeting("Android")
+fun SplashScreen(navController: NavHostController) {
+    var scale by remember { mutableStateOf(1f) }
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(true) {
+        scale = 1.2f
+        delay(2000) // Hiển thị trong 2 giây
+        coroutineScope.launch {
+            navController.navigate("greeting") {
+                popUpTo("splash") { inclusive = true }
+            }
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        // Ảnh nền
+        Image(
+            painter = painterResource(id = com.fourstars.fourstars_english.R.drawable.logo),
+            contentDescription = "App Logo",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.size(250.dp)
+        )
+    }
+}
+
+@Composable
+fun GreetingScreen(navController: NavHostController) {
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(12.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.greeting),
+            contentDescription = "Ảnh chào mừng",
+            contentScale = ContentScale.FillWidth,
+            modifier = Modifier
+                .size(350.dp)
+        )
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Text(
+            text = "Learn Japanese easily and effectively with Torii!",
+            fontSize = 34.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = Nunito,
+            textAlign = TextAlign.Center,
+            color = Color.DarkGray,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+
+        Spacer(modifier = Modifier.height(60.dp))
+
+        // Nút "Tiếp theo" hoặc "Vào ứng dụng"
+        val coroutineScope = rememberCoroutineScope()
+
+        Button(
+            onClick = {
+                coroutineScope.launch {
+                    navController.navigate("register")
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp),
+            shape = RoundedCornerShape(15.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF58CC02),
+            ),
+        ) {
+            Text(
+                text = "GET STARTED",
+                fontSize = 16.sp,
+                fontFamily = Feather,
+            )
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            onClick = {
+                coroutineScope.launch {
+                    navController.navigate("login")
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp),
+            shape = RoundedCornerShape(15.dp),
+            colors = ButtonDefaults.outlinedButtonColors(
+                containerColor = Color.White, // Nền trắng
+                contentColor = Color(0xFF58CC02)
+            ),
+            border = BorderStroke(2.dp, Color.LightGray),
+        ) {
+            Text(
+                text = "I ALREADY HAVE AN ACCOUNT",
+                fontSize = 16.sp,
+                fontFamily = Feather,
+            )
+        }
     }
 }
--- app/src/main/java/com/fourstars/fourstars_english/NavGraph.kt
+++ app/src/main/java/com/fourstars/fourstars_english/NavGraph.kt
@@ -1,2 +1,108 @@
 package com.fourstars.fourstars_english
 
+import android.os.Build
+import android.window.SplashScreen
+import androidx.annotation.RequiresApi
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.fourstars.fourstars_english.repository.AuthRepository
+import com.fourstars.fourstars_english.repository.GoogleAuthRepository
+import com.fourstars.fourstars_english.screens.auth.LoginScreen
+import com.fourstars.fourstars_english.screens.auth.RegisterScreen
+import com.fourstars.fourstars_english.screens.home.articles.ArticleDetailScreen
+import com.fourstars.fourstars_english.screens.home.articles.ArticlesScreen
+import com.fourstars.fourstars_english.screens.home.video.VideoScreen
+import com.fourstars.fourstars_english.screens.home.words.CategoryScreen
+import com.fourstars.fourstars_english.screens.home.words.VocabularyScreen
+import com.fourstars.fourstars_english.screens.main.CommunityScreen
+import com.fourstars.fourstars_english.screens.main.HomeScreen
+import com.fourstars.fourstars_english.screens.main.LearningScreen
+import com.fourstars.fourstars_english.screens.main.SearchScreen
+import com.fourstars.fourstars_english.screens.search.GrammarScreen
+import com.fourstars.fourstars_english.screens.study.FlashcardScreen
+import com.fourstars.fourstars_english.screens.study.NotebookDetailScreen
+import com.fourstars.fourstars_english.screens.study.QuizScreen
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun AppNavGraph(navController: NavHostController) {
+    val navController = rememberNavController()
+    val authRepo = AuthRepository()
+    val context = LocalContext.current
+    val googleAuthRepo = GoogleAuthRepository(context)
+
+    NavHost(navController = navController, startDestination = "splash") {
+        composable("login") { LoginScreen(navController, authRepo, googleAuthRepo) }
+        composable("register") { RegisterScreen(navController, authRepo, googleAuthRepo) }
+        composable("splash") { SplashScreen(navController) }
+        composable("greeting") { GreetingScreen(navController) }
+        composable("home") { HomeScreen(
+            navController,
+            authRepo
+        ) }
+        composable("search") { SearchScreen(navController) }
+        composable("learning") { LearningScreen(navController) }
+        composable("community") { CommunityScreen(navController) }
+        composable("category") { CategoryScreen(navController) }
+        composable("vocabulary/{category}") { backStackEntry ->
+            val category = backStackEntry.arguments?.getString("category") ?: ""
+            VocabularyScreen(navController, category)
+        }
+        composable("articles") { ArticlesScreen(navController) }
+        composable(
+            route = "article_detail/{title}/{publishDate}/{content}/{imageUrl}/{audioUrl}",
+            arguments = listOf(
+                navArgument("title") { type = NavType.StringType },
+                navArgument("publishDate") { type = NavType.StringType },
+                navArgument("content") { type = NavType.StringType },
+                navArgument("imageUrl") { type = NavType.StringType },
+                navArgument("audioUrl") { type = NavType.StringType }
+            )
+        ) { backStackEntry ->
+            val title = backStackEntry.arguments?.getString("title") ?: ""
+            val publishDate = backStackEntry.arguments?.getString("publishDate") ?: ""
+            val content = backStackEntry.arguments?.getString("content") ?: ""
+            val imageUrl = backStackEntry.arguments?.getString("imageUrl") ?: ""
+            val audioUrl = backStackEntry.arguments?.getString("audioUrl") ?: ""
+            ArticleDetailScreen(title, publishDate, content, imageUrl, audioUrl, navController)
+        }
+        composable("video/{videoId}") { backStackEntry ->
+            val videoId = backStackEntry.arguments?.getString("videoId") ?: return@composable
+            VideoScreen(navController, videoId = videoId)
+        }
+        composable("videos") { VideoScreen(navController) }
+        composable("grammar") { GrammarScreen(navController) }
+
+        // Notebook routes
+        composable(
+            route = "notebook_detail/{notebookId}",
+            arguments = listOf(
+                navArgument("notebookId") { type = NavType.StringType }
+            )
+        ) { backStackEntry ->
+            val notebookId = backStackEntry.arguments?.getString("notebookId") ?: ""
+            NotebookDetailScreen(navController, notebookId)
+        }
+        composable("flashcard/{notebookId}") { backStackEntry ->
+            val notebookId = backStackEntry.arguments?.getString("notebookId") ?: ""
+            FlashcardScreen(navController, notebookId)
+        }
+
+        composable(
+            route = "notebook/{notebookId}/quiz",
+            arguments = listOf(navArgument("notebookId") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val notebookId = backStackEntry.arguments?.getString("notebookId") ?: return@composable
+            QuizScreen(
+                notebookId = notebookId,
+                onBack = { navController.popBackStack() }
+            )
+        }
+    }
+}
